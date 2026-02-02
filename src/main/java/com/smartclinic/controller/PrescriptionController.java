c4873273d94f7d041840a732ca425930f9c1b7dd
dilipmettur <dilipmettur@gmail.com>
2026-02-02 19:35:30 +0530

    PrescriptionController.java

---
+++ src/main/java/com/smartclinic/controller/PrescriptionController.java
@@ -0,0 +1,19 @@
+package com.smartclinic.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+
+    @PostMapping("/issue")
+    public Map<String, String> issuePrescription(@RequestParam int appointmentId, @RequestBody String medicineDetails) {
+        // Logic to link prescription to appointment
+        Map<String, String> response = new HashMap<>();
+        response.setStatus("SUCCESS");
+        response.put("message", "Prescription issued for Appointment ID: " + appointmentId);
+        return response;
+    }
+}
