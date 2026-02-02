aed9aac4db300fa75a06bc52a99599965ad5ac69
dilipmettur <dilipmettur@gmail.com>
2026-02-02 19:34:06 +0530

    TokenService.java

---
+++ src/main/java/com/smartclinic/service/TokenService.java
@@ -0,0 +1,24 @@
+package com.smartclinic.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class TokenService {
+    private String secret = "smart_clinic_secret_key_2026"; // In production, use an environment variable
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+}
